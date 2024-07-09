853d7b5ad946bab0439d402dae95d06b1ce7374e
Kamil Kępski <kamil.kepskii@gmail.com>
2024-07-10 00:04:47 +0200

    NavigationView update, ClientPage and RecipientPage added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,32 +1,37 @@
+using System;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Windowing;
+using Windows.ApplicationModel;
 
 namespace kInvoice
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-            this.AppWindow.Title = "kInv - darmowy program do wystawiania faktur";
+            this.AppWindow.Title = "kInvoice - darmowy program do wystawiania faktur";
+            MainNavigationView.SelectionChanged += MainNavigationView_SelectionChanged;
         }
+
+        private void MainNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavigationViewItem selectedItem = args.SelectedItem as NavigationViewItem;
+            if (selectedItem != null)
+            {
+                switch (selectedItem.Name)
+                {
+                    case "ClientPage":
+                        ContentFrame.Navigate(typeof(Views.ClientPage));
+                        break;
+                    case "RecipientPage":
+                        ContentFrame.Navigate(typeof(Views.RecipientPage));
+                        break;
+                }
+            }
+        }
+
     }
 }
